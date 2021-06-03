da65c466387a2fa825e633486bcfbfe89414b84a
nhta2282001 <85274721+nhta2282001@users.noreply.github.com>
2021-06-03 14:48:38 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,8 +23,8 @@ function firstQuestion(){
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
         imageUrl: 'img/lookMe.jpg',
-        imageWidth: 300,
-        imageHeight: 300,
+        imageWidth: 500,
+        imageHeight: 500,
         background: '#fff url("img/iput-bg.jpg")',
         imageAlt: 'Custom image',
         confirmButtonText: CONFIG.btnIntro
